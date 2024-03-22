18e8539229aeb1311b4d01a6a21837166ba7769a
fohy24 <fohoyin@gmail.com>
2024-03-22 14:41:22 -0700

    working deployment

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def clean_data(dirty_df):
 #     options.add_argument('--no-sandbox')
 #     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-@st.experimental_singleton
+@st.cache_resource
 def installff():
   os.system('sbase install geckodriver')
   os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
