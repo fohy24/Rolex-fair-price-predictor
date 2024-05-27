ce9bea16d32b7918007a5116c08b426b19f27ef6
fohy24 <fohoyin@gmail.com>
2024-05-26 19:24:06 -0700

    commented out manual input tab

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from bs4 import BeautifulSoup
 
 model = pickle.load(open('model/xgboost_opt.pkl', 'rb'))
 url_data = {}
-df = pd.read_csv('data/df.csv').drop(columns=["price"]).dropna()
+df = pd.read_csv('data/rolex_df.csv').drop(columns=["price"])
 fields = df.columns.to_list()
 
 def is_convertible_to_int(value):
@@ -223,79 +223,79 @@ def main():
     st.header("Rolex Fair Price Predictor")
     st.write("Enter the specification and predict the price of a Rolex watch!")
     # st.sidebar.radio('drops sub-menu', options=['add drops', 'view drops'])
-    tab1, tab2 = st.tabs(["Predict from URL", "Manual Input"])
+    tab1 = st.tabs(["Predict from URL"])
 
-    with tab1:
+    # with tab1:
         # st.subheader("Paste a link below to predict the price!")
-        url = st.text_input(
-        "Paste a link from [Chrono24.com](https://www.chrono24.ca/rolex/index-1.htm?pageSize=120&showpage=1) to predict the price!",
-        label_visibility="visible",
-        disabled=False,
-        placeholder="https://www.chrono24.ca/rolex/rolex-gmt-master-ii--id24333283.htm",
-        )
-        if st.button("Predict from URL"):
-            st.toast("Fetching data...")
-            user_selections_df = get_data_from_url(url)
-            user_selections_df.replace('', None, inplace=True)
-            prediction = model.predict(user_selections_df)
+    url = st.text_input(
+    "Paste a link from [Chrono24.com](https://www.chrono24.ca/rolex/index-1.htm?pageSize=120&showpage=1) to predict the price!",
+    label_visibility="visible",
+    disabled=False,
+    placeholder="https://www.chrono24.ca/rolex/rolex-gmt-master-ii--id24333283.htm",
+    )
+    if st.button("Predict from URL"):
+        st.toast("Fetching data...")
+        user_selections_df = get_data_from_url(url)
+        user_selections_df.replace('', None, inplace=True)
+        prediction = model.predict(user_selections_df)
 
-            st.success(f'The fair price of this Rolex is estimated to be CA${prediction[0]:,.0f}')
+        st.success(f'The fair price of this Rolex is estimated to be CA${prediction[0]:,.0f}')
 
-    with tab2:
+    # with tab2:
 
-        slider_configs = {
-            'rating': {'range': (1.0, 5.0), 'step': 0.1, 'default': float(np.median(df['rating'].dropna()))},
-            'case_diameter': {'range': (13, 50), 'step': 1, 'default': int(np.median(df['case_diameter'].dropna()))},
-            'reviews': {'range': (0, 10000), 'step': 50, 'default': int(np.median(df['reviews'].dropna()))},
-        }
+    #     slider_configs = {
+    #         'rating': {'range': (1.0, 5.0), 'step': 0.1, 'default': float(np.median(df['rating'].dropna()))},
+    #         'case_diameter': {'range': (13, 50), 'step': 1, 'default': int(np.median(df['case_diameter'].dropna()))},
+    #         'reviews': {'range': (0, 10000), 'step': 50, 'default': int(np.median(df['reviews'].dropna()))},
+    #     }
 
-        # Binary fields and default values
-        binary_options = {
-            'year_is_approximated': {'options': ['Yes', 'No'], 'default': 'No'},
-            'is_negotiable': {'options': ['Yes', 'No'], 'default': 'No'}
-        }
+    #     # Binary fields and default values
+    #     binary_options = {
+    #         'year_is_approximated': {'options': ['Yes', 'No'], 'default': 'No'},
+    #         'is_negotiable': {'options': ['Yes', 'No'], 'default': 'No'}
+    #     }
 
-        unsorted_fields = ['condition', 'scope_of_delivery', 'country', 'availability']
+    #     unsorted_fields = ['condition', 'scope_of_delivery', 'country', 'availability']
 
-        # Generate selectbox options with default values
-        selectbox_options = {}
-        for field in fields:
-            friendly_name = ' '.join(word.capitalize() for word in field.split('_'))
-            if field in binary_options:
-                options = binary_options[field]['options']
-                default = binary_options[field]['default']
-                selectbox_options[friendly_name] = (field, options, options.index(default))
-            elif field in unsorted_fields:
-                options = df[field].unique().tolist()
-                selectbox_options[friendly_name] = (field, options, 0) 
-            else:
-                options = sorted(df[field].unique().tolist())
-                selectbox_options[friendly_name] = (field, options, 0)
+    #     # Generate selectbox options with default values
+    #     selectbox_options = {}
+    #     for field in fields:
+    #         friendly_name = ' '.join(word.capitalize() for word in field.split('_'))
+    #         if field in binary_options:
+    #             options = binary_options[field]['options']
+    #             default = binary_options[field]['default']
+    #             selectbox_options[friendly_name] = (field, options, options.index(default))
+    #         elif field in unsorted_fields:
+    #             options = df[field].unique().tolist()
+    #             selectbox_options[friendly_name] = (field, options, 0) 
+    #         else:
+    #             options = sorted(df[field].unique().tolist())
+    #             selectbox_options[friendly_name] = (field, options, 0)
 
-        user_selections = {}
+    #     user_selections = {}
 
-        for category, (original_field_name, options, default_index) in selectbox_options.items():
-            if original_field_name in slider_configs:
-                config = slider_configs[original_field_name]
-                selected_option = st.slider(category, min_value=config['range'][0], max_value=config['range'][1],
-                                            value=config['default'], step=config['step'])
-            else:
-                selected_option = st.selectbox(category, options, index=default_index)
-            user_selections[original_field_name] = selected_option
+    #     for category, (original_field_name, options, default_index) in selectbox_options.items():
+    #         if original_field_name in slider_configs:
+    #             config = slider_configs[original_field_name]
+    #             selected_option = st.slider(category, min_value=config['range'][0], max_value=config['range'][1],
+    #                                         value=config['default'], step=config['step'])
+    #         else:
+    #             selected_option = st.selectbox(category, options, index=default_index)
+    #         user_selections[original_field_name] = selected_option
 
 
-        if st.button("Predict from manual inputs"):
-            # Convert user selections to DataFrame for prediction
-            user_selections_df = pd.DataFrame([user_selections])
+    #     if st.button("Predict from manual inputs"):
+    #         # Convert user selections to DataFrame for prediction
+    #         user_selections_df = pd.DataFrame([user_selections])
 
-            # Convert back to 1 and 0
-            user_selections_df['year_is_approximated'] = user_selections_df['year_is_approximated'].map({'Yes': 1, 'No': 0})
-            user_selections_df['is_negotiable'] = user_selections_df['is_negotiable'].map({'Yes': 1, 'No': 0})
-            # print(user_selections_df)
+    #         # Convert back to 1 and 0
+    #         user_selections_df['year_is_approximated'] = user_selections_df['year_is_approximated'].map({'Yes': 1, 'No': 0})
+    #         user_selections_df['is_negotiable'] = user_selections_df['is_negotiable'].map({'Yes': 1, 'No': 0})
+    #         # print(user_selections_df)
 
-            prediction = model.predict(user_selections_df)
+    #         prediction = model.predict(user_selections_df)
 
-            st.success(f'This Rolex is estimated to sell for CA${prediction[0]:,.0f}')
+    #         st.success(f'This Rolex is estimated to sell for CA${prediction[0]:,.0f}')
 
       
 if __name__=='__main__': 
